a2f56de57518d300a1ce7057d997ecc58205665d
Nilay Savant <nilayofficial@gmail.com>
2024-03-23 21:26:32 +0530

    Add dummy test comp, to test closures for builders.

--- crates/parametric_cad_modelling/src/bevy_plugin/components/mod.rs
+++ crates/parametric_cad_modelling/src/bevy_plugin/components/mod.rs
@@ -2,6 +2,8 @@
 pub mod cad;
 /// Components used to mark and config camera used for CAD generation.
 pub mod camera;
+/// Dummy Test Components
+pub mod dummy_test;
 /// Components used in Params UI.
 pub mod params_ui;
 /// Components used for wire frame display.
