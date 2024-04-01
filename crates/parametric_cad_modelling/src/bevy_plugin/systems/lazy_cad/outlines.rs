6efc5901d0088219e806c290bdbeb3a7c8ecc9b3
Nilay Savant <nilayofficial@gmail.com>
2024-04-01 23:39:38 +0530

    Fix outlines for meshes.

--- crates/parametric_cad_modelling/src/bevy_plugin/systems/lazy_cad/outlines.rs
+++ crates/parametric_cad_modelling/src/bevy_plugin/systems/lazy_cad/outlines.rs
@@ -8,7 +8,7 @@ pub fn generate_mesh_outlines(
     cad_meshes: Query<
         (
             Entity,
-            &Transform,
+            &GlobalTransform,
             &CadGeneratedMeshOutlinesState,
             &CadGeneratedMeshOutlines,
         ),
@@ -16,7 +16,7 @@ pub fn generate_mesh_outlines(
     >,
     mut gizmos: Gizmos,
 ) {
-    for (cad_mesh_ent, transform, outlines_state, CadGeneratedMeshOutlines(line_strip_positions)) in
+    for (cad_mesh_ent, glob_transform, outlines_state, CadGeneratedMeshOutlines(line_strip_positions)) in
         cad_meshes.iter()
     {
         let color = match *outlines_state {
@@ -28,7 +28,7 @@ pub fn generate_mesh_outlines(
         for positions in line_strip_positions.iter() {
             let mut positions = positions.clone();
             positions.iter_mut().for_each(|p| {
-                *p = transform.transform_point(*p);
+                *p = glob_transform.transform_point(*p);
             });
             if positions.len() > 2 {
                 positions.push(*positions.first().unwrap());
