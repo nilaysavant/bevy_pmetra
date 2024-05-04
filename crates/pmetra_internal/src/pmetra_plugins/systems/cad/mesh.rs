b448a07831ae928c4754395c0a154a66ef2bf419
Nilay Savant <nilayofficial@gmail.com>
2024-05-04 14:31:46 +0530

    WIP

--- crates/pmetra_internal/src/pmetra_plugins/systems/cad/mesh.rs
+++ crates/pmetra_internal/src/pmetra_plugins/systems/cad/mesh.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::*;
-use bevy_mod_picking::prelude::*;
+use bevy::{prelude::*, utils::HashSet};
+use bevy_mod_picking::{pointer::InputPress, prelude::*};
 
 use crate::pmetra_plugins::components::cad::{
     BelongsToCadGeneratedRoot, CadGeneratedMesh, CadGeneratedRoot, CadGeneratedRootSelectionState,
@@ -41,20 +41,49 @@ pub fn mesh_pointer_out(
 
 pub fn update_root_selection_based_on_mesh_selection(
     mut cad_generated: Query<(Entity, &mut CadGeneratedRootSelectionState), With<CadGeneratedRoot>>,
-    cad_meshes: Query<(Entity, &PickSelection, &BelongsToCadGeneratedRoot), With<CadGeneratedMesh>>,
+    cad_meshes: Query<&BelongsToCadGeneratedRoot, With<CadGeneratedMesh>>,
+    mut pointer_down: EventReader<Pointer<Down>>,
+    mut presses: EventReader<InputPress>,
+    mut selections: EventReader<Pointer<Select>>,
 ) {
-    for (root_ent, mut root_selection_state) in cad_generated.iter_mut() {
-        let any_mesh_selected =
-            cad_meshes
-                .iter()
-                .any(|(_, selection, &BelongsToCadGeneratedRoot(cur_root_ent))| {
-                    cur_root_ent == root_ent && selection.is_selected
-                });
-        if any_mesh_selected {
+    // Check for selections, if a mesh is selected, set the root to selected.
+    for selection_event in selections.read() {
+        if let Ok(&BelongsToCadGeneratedRoot(root_ent)) = cad_meshes.get(selection_event.target) {
+            let Ok((_, mut root_selection_state)) = cad_generated.get_mut(root_ent) else {
+                continue;
+            };
             *root_selection_state = CadGeneratedRootSelectionState::Selected;
-        } else if !matches!(*root_selection_state, CadGeneratedRootSelectionState::Hovered) {
-            // Only set to None if not already in hovered. Otherwise it wont show hovered (will override).
-            *root_selection_state = CadGeneratedRootSelectionState::None;
+        }
+    }
+
+    // Following is borrowed from `bevy_mod_picking`: https://github.com/aevyrie/bevy_mod_picking/blob/0af5d0c80cd027c74373e74bbfe143119f791c06/crates/bevy_picking_selection/src/lib.rs#L155-L214
+    // Used to de-select all root entities if a pointer has clicked on nothing...
+
+    // Pointers that have clicked on something.
+    let mut pointer_down_list = HashSet::new();
+
+    for Pointer {
+        pointer_id,
+        pointer_location,
+        target,
+        event: _,
+    } in pointer_down
+        .read()
+        .filter(|pointer| pointer.event.button == PointerButton::Primary)
+    {
+        pointer_down_list.insert(pointer_id);
+    }
+    // If a pointer has pressed, but did not press on anything, this means it clicked on nothing. If
+    // so, and the setting is enabled, deselect everything.
+    for press in presses
+        .read()
+        .filter(|p| p.is_just_down(PointerButton::Primary))
+    {
+        let id = press.pointer_id;
+        if !pointer_down_list.contains(&id) {
+            for (root_ent, mut root_selection_state) in cad_generated.iter_mut() {
+                *root_selection_state = CadGeneratedRootSelectionState::None;
+            }
         }
     }
 }
